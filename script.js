10077bc451cf7a9f1109ea8b48a640fc4ee41b50
Sandra Katsikis <mindfulripples@gmail.com>
2023-09-10 21:15:40 +1000

    fetch api

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content added by JavaScript plus change";
 document.querySelector("body").appendChild(h2);
+
+
+fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+.then(response => response.json())
+.then(data =>console.log(data))
