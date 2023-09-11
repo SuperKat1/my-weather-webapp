b8eb7cd7e9a8bae052c4bece61fbc30c9bff9de0
Sandra Katsikis <mindfulripples@gmail.com>
2023-09-11 10:44:54 +1000

    create MVP form

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript plus change";
-document.querySelector("body").appendChild(h2);
+const button = document.querySelector(".button")
+const inputValue = document.querySelector(".inputValue")
+const cityName = document.querySelector(".cityName")
+const desc = document.querySelector(".desc")
+const temperature = document.querySelector(".temperature")
 
-
-fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+button.addEventListener('click', function(){
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
 .then(response => response.json())
-.then(data =>console.log(data))
+.then(data => {
+    const cityNameValue = data['cityName'];
+    const temperatureValue = data['main']['temperature'];
+    const descValue = data['weather'][0]['description'];
+
+    cityName.innerHTML = cityNameValue;
+    temperatureValue.innerHTML = temperatureValue;
+    descValue.innerHTML = descValue;
+})
+
+.catch(err => alert("City name not found"))
+})
